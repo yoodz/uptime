d136c4f545baca292113869da2046a1cca82fb81
zhengdayong <developer.vip@outlook.com>
2023-07-29 17:50:54 +0800

    init

---
+++ config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '服务状态',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2240047-7e3dc91a209ade0bbe2a3648',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'afunny.top',
+      url: 'https://www.afunny.top/'
+    },
+  ],
+};
