3150f4f5e459e21f8aed57dd30246bd5405fa5e6
dayong <904201050@qq.com>
2024-09-10 16:47:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,8 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'm794906385-58a6d2d02da9fe99f6d2af26',
-    'm794907541-e7df53ccbd4280278a4a7061'
+    'm794907541-e7df53ccbd4280278a4a7061',
+    'm797651239-533fecbb124d51ea4a183485'
   ],
 
   // 日志天数
